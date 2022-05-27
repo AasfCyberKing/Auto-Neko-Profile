27c582bd41e4b0823681387613764cefd022ef46
Aasf-Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-05-27 21:41:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import logging
 from pyrogram import Client, idle
 from asyncio import get_event_loop
-import requests
 import time
 
 logging.basicConfig(
@@ -11,7 +10,7 @@ logging.basicConfig(
 
 API_ID = os.environ.get("API_ID", None) 
 API_HASH = os.environ.get("API_HASH", None) 
-SESSION = "BQDBFePXlgEzIB6b5e1WPvEQFAqK4FLN1b4pr6C0hPM08dZb9QNXcC03aIVO_uUwy7nnw64eBD6NYpvsIM6OUirApKN9DuLbbJUJPmoj2mNi6aaXWq_ogExhR4U0t3a71p0ujz-_4TkccxSOUVEeoqAWVnPr8qANJD9s3S5izOwOHn3s1QXgNvudGx2pzWF9A2069NzKKltfhY3z9QsQWnDZFE0pGBDxUkSxvSJnlyqD2X645Vrbgjox7qm9_bA-t8cZejBeOKNt3NT4ZSWGb3K1skUmizPlNnS6_kDIn4FiSkCoYu-FM3cx6nt87_cMBXhJUoumaWUZAq5aQXI_ePh8AAAAAT1MlXEA"
+SESSION = os.environ.get("SESSION", None) 
 USERNAME = os.environ.get("USERNAME", None) 
 
 # For Local Deploy:
@@ -30,10 +29,6 @@ ubot = Client(
 
 async def main():
     await ubot.start()
-    url = "https://nekos.best/api/v2/neko"
-    r = requests.get(url)
-    e = r.json()
-    pics = e["results"][0]["url"]
     await ubot.send_message("@botfather", "/setuserpic")
     time.sleep(5)
     await ubot.send_message("@botfather", f"@{USERNAME}")
